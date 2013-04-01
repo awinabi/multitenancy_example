5d3e518e612951d2fa32841fec2b99ba223ec02e
awinabi <awin.abi@strata.co.in>
2013-04-02 00:00:39 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Multi-tenancy example in rails 3 using milia gem
+- milia gem ~ https://github.com/dsaronin/milia
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MultitenancyExample::Application.config.secret_token = '545ec28ef6bd2b0e23d18f17452657f9b007e0e401f7905f2d4ef8186289e5c2fe843969d7bf87b5c2ee1f8954bfcebacfc73a8107e00336f149d743f60c2eb7'
