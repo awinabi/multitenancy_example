b986db81a67ddb8dac03cc5875ec22d804369d57
awinabi <awin.abi@strata.co.in>
2013-04-02 15:06:46 +0530

    milia: applicaton_controller changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,34 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :authenticate_user!
+  # before_filter :authenticate_user!
+
+  before_filter :authenticate_tenant!   # authenticate user and setup tenant
+
+  # ------------------------------------------------------------------------------
+  # authenticate_tenant! -- authorization & tenant setup
+  # -- authenticates user
+  # -- sets current tenant
+  # -- sets up app environment for this user
+  # ------------------------------------------------------------------------------
+  def authenticate_tenant!()
+
+    unless authenticate_user!
+      email = ( params.nil? || params[:user].nil?  ?  ""  : " as: " + params[:user][:email] )
+
+      flash[:notice] = "cannot sign you in#{email}; check email/password and try again"
+
+      return false  # abort the before_filter chain
+    end
+
+    # user_signed_in? == true also means current_user returns valid user
+    raise SecurityError,"*** invalid sign-in  ***" unless user_signed_in?
+
+    set_current_tenant   # relies on current_user being non-nil
+
+    # any application-specific environment set up goes here
+
+    true  # allows before filter chain to continue
+  end
+
 
 end
